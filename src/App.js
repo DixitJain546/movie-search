d6098fe62b632a456d89c53be9adfde0ce0147bd
dixitjain <dixit.jain@gdn-commerce.com>
2025-04-28 20:41:44 +0530

    adding css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import MovieSearch from './containers/MovieSearch';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-container">
+      <header className="app-header">
+        <h1>Movie Search App</h1>
       </header>
+      <main>
+        <MovieSearch />
+      </main>
+      <footer className="app-footer">
+        <p>&copy; 2025 Movie Search App | Powered by OMDB API</p>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
